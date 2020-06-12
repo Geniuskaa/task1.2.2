7b531433e48d507b4e404edbcc4dee1b22620838
Geniuska Geniusovna <eminizinar@gmail.com>
2020-06-12 09:50:15 +0300

    Main.go modified

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 func main() {
 	var balance int32 = 0 // 15 миллионов в копейках
 	var transfer int32 = 0 // 10 миллионов в копейках
-	total := balance + transfer // int32 + int32 будет int32
+	rubBalance := balance / 100
+	rubTransfer := transfer / 100
+	total := rubBalance + rubTransfer // int32 + int32 будет int32
 	println(total)
 }
